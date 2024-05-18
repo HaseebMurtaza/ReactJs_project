384bbb68c5392ad24545ac82d610502f7be0e3cc
Haseeb Murtaza <haseebmurtaza558@gmail.com>
2024-05-19 04:25:21 +0500

    React hooks,Search,LiveAPI,Filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,61 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setlistOfRestaurant] = useState(resList);
+  const [listOfRestaurants, setlistOfRestaurant] = useState([]);
+  const [filteredRestaurants, setfilteredRestaurants] = useState([]);
 
-  return (
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body Rendered");
+
+  useEffect(() => {
+    fecthData();
+  }, []);
+
+  const fecthData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&collection=83644&tags=layout_CCS_Pizza&sortBy=&filters=&type=rcv2&offset=0&page_type=nulls"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+    setlistOfRestaurant(resList);
+    setfilteredRestaurants(resList);
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            placeholder="Search Restaurant"
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setfilteredRestaurants(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -21,7 +69,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,7 +8,7 @@ const RestaurantCard = (props) => {
     cuisines,
     avgRating,
     costForTwo,
-    deliveryTime,
+    // deliveryTime,
   } = resData?.data;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
@@ -21,7 +21,7 @@ const RestaurantCard = (props) => {
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
+      {/* <h4>{deliveryTime} minutes</h4> */}
     </div>
   );
 };
