c4a56c143152aa9cbe7a4a7350c8252c4be88ede
Haseeb Murtaza <haseebmurtaza245@gmail.com>
2024-06-18 00:03:45 +0500

    Higher order compoenet, controlled&uncontrolled component, lifting state up, build accordian

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data }) => {
+const RestaurantCategory = ({ data, showItems, setshowIndex }) => {
+  const handleClick = () => {
+    setshowIndex();
+  };
   return (
     <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 ">
       {/*accordian Header */}
 
-      <div className="flex justify-between">
+      <div
+        className="flex justify-between cursor-pointer"
+        onClick={handleClick}
+      >
         <span className="font-bold text-md">
           {data.title} ({data.itemCards.length})
         </span>
         <span>⬇️</span>
       </div>
-      {/*accordian Body */}
-      <ItemList items={data.itemCards} />
+
+      {showItems && <ItemList items={data.itemCards} />}
     </div>
   );
 };
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -3,11 +3,14 @@ import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
 import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const resInfo = useRestaurantMenu(resId);
+
+  const [showIndex, setshowIndex] = useState(0);
 
   if (resInfo === null) return <Shimmer />;
 
@@ -17,16 +20,12 @@ const RestaurantMenu = () => {
   const { itemCards } =
     resInfo?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
-  // console.log(resInfo?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards);
-
   const categories =
     resInfo?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.["card"]?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
-
-  // console.log(categories);
 
   return (
     <div className=" mx-10 py-6 text-center">
@@ -35,22 +34,16 @@ const RestaurantMenu = () => {
         {cuisines.join(",")} - {costForTwoMessage}
       </p>
       {/* Categories accordians*/}
-      {categories.map((category) => (
+      {categories.map((category, index) => (
         <RestaurantCategory
-          data={category?.card?.card}
           key={category?.card?.card.title}
+          data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setshowIndex={() => {
+            setshowIndex(index);
+          }}
         />
       ))}
-
-      {/* <h2 className=" px-2 py-2 font-bold text-lg">Menu</h2>
-      <ul className="px-2 py-2 font-mono">
-        {itemCards.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} : {"Rs"}{" "}
-            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-          </li>
-        ))}
-      </ul> */}
     </div>
   );
 };
